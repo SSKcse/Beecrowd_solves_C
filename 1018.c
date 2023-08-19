e95f70bea016c1d53847d95c1d3b1aa3ffe87448
Shuvo Karmaker <142686052+SSKcse@users.noreply.github.com>
2023-08-20 01:49:40 +0600

    Update 1018.c

--- 1018.c
+++ 1018.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 
- int main()
- {
-  int nots;
 
-  scanf("%d", &nots);
+ int main(){
+    int notes, aux;
 
-  int hundred;
+    scanf("%d", &notes);
 
-  hundred= nots/100;
-  s = s - (h*3600);
+    printf("%d\n", notes);
+    printf("%d nota(s) de R$ 100,00\n", notes/100);
+    aux = (notes%100);
 
-  int m;
 
-  m= s/60;
-  s = s - (m*60);
+    printf("%d nota(s) de R$ 50,00\n", aux/50);
+    aux = (aux%50);
 
-  printf("%d:%d:%d\n",h,m,s);
-  return 0;
+    printf("%d nota(s) de R$ 20,00\n", aux/20);
+    aux = (aux%20);
+
+    printf("%d nota(s) de R$ 10,00\n", aux/10);
+    aux = (aux%10);
+
+    printf("%d nota(s) de R$ 5,00\n", aux/5);
+    aux = (aux%5);
+
+    printf("%d nota(s) de R$ 2,00\n", aux/2);
+    aux = (aux%2);
+
+    printf("%d nota(s) de R$ 1,00\n", aux/1);
+    return 0;
  }
+
