d4d629d2584ff2765c05c0c54e451021bd9a7c2d
Shuvo Karmaker <142686052+SSKcse@users.noreply.github.com>
2023-08-19 23:33:57 +0600

    Add files via upload

---
+++ 1018.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+ int main()
+ {
+  int nots;
+
+  scanf("%d", &nots);
+
+  int hundred;
+
+  hundred= nots/100;
+  s = s - (h*3600);
+
+  int m;
+
+  m= s/60;
+  s = s - (m*60);
+
+  printf("%d:%d:%d\n",h,m,s);
+  return 0;
+ }
